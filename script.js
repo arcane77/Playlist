3f9f1205d591e8ed803ed6b2e5bd7ceaf1ed6d6d
arcane77 <96630482+arcane77@users.noreply.github.com>
2022-01-27 16:50:52 +0530

    js file imported

---
+++ script.js
@@ -0,0 +1,154 @@
+var song1 = document.getElementById("song1");
+var icon0 = document.getElementById("icon0");
+icon0.onclick = function(){
+    if(song1.paused){
+        song1.play();
+        icon0.src = "icons8-pause-100.png";
+        
+    }else{
+        song1.pause();
+        icon0.src = "icons8-play-100.png";
+        
+       
+    }
+}
+
+var song2 = document.getElementById("song2");
+var icon1 = document.getElementById("icon1");
+icon1.onclick = function(){
+    if(song2.paused){
+        song2.play();
+        icon1.src = "icons8-pause-100.png";
+        
+    }else{
+        song2.pause();
+        icon1.src = "icons8-play-100.png";
+        
+    }
+}
+
+var song3 = document.getElementById("song3");
+    var icon2 = document.getElementById("icon2");
+    icon2.onclick = function(){
+        if(song3.paused){
+            song3.play();
+            icon2.src = "icons8-pause-100.png";
+            
+        }else{
+            song3.pause();
+            icon2.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song4 = document.getElementById("song4");
+    var icon3 = document.getElementById("icon3");
+    icon3.onclick = function(){
+        if(song4.paused){
+            song4.play();
+            icon3.src = "icons8-pause-100.png";
+            
+        }else{
+            song4.pause();
+            icon3.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song5 = document.getElementById("song5");
+    var icon4 = document.getElementById("icon4");
+    icon4.onclick = function(){
+        if(song5.paused){
+            song5.play();
+            icon4.src = "icons8-pause-100.png";
+            
+        }else{
+            song5.pause();
+            icon4.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song6 = document.getElementById("song6");
+    var icon5 = document.getElementById("icon5");
+    icon5.onclick = function(){
+        if(song6.paused){
+            song6.play();
+            icon5.src = "icons8-pause-100.png";
+            
+        }else{
+            song6.pause();
+            icon5.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song7 = document.getElementById("song7");
+    var icon6 = document.getElementById("icon6");
+    icon6.onclick = function(){
+        if(song7.paused){
+            song7.play();
+            icon6.src = "icons8-pause-100.png";
+            
+        }else{
+            song7.pause();
+            icon6.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song8 = document.getElementById("song8");
+    var icon7 = document.getElementById("icon7");
+    icon7.onclick = function(){
+        if(song8.paused){
+            song8.play();
+            icon7.src = "icons8-pause-100.png";
+            
+        }else{
+            song8.pause();
+            icon7.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song9 = document.getElementById("song9");
+    var icon8 = document.getElementById("icon8");
+    icon8.onclick = function(){
+        if(song9.paused){
+            song9.play();
+            icon8.src = "icons8-pause-100.png";
+            
+        }else{
+            song9.pause();
+            icon8.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song10 = document.getElementById("song10");
+    var icon9 = document.getElementById("icon9");
+    icon9.onclick = function(){
+        if(song10.paused){
+            song10.play();
+            icon9.src = "icons8-pause-100.png";
+            
+        }else{
+            song10.pause();
+            icon9.src = "icons8-play-100.png";
+            
+        }
+    }
+
+    var song11 = document.getElementById("song11");
+    var icon10 = document.getElementById("icon10");
+    icon10.onclick = function(){
+        if(song11.paused){
+            song11.play();
+            icon10.src = "icons8-pause-100.png";
+            
+        }else{
+            song11.pause();
+            icon10.src = "icons8-play-100.png";
+            
+        }
+    }
